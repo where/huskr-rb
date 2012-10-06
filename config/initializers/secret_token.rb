8c9fa1ef57dac2b11fd19dd0a0e362345d558b70
Ken Mazaika <kenmazaika@gmail.com>
2012-10-06 17:13:43 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Huskr::Application.config.secret_token = 'a6825a7a4cfcd95fe981ab6da3129ab81194064a8b96b0bfde6ccdc2feaa07cb696c6e7c362b3132aab664cc6f98fa4fd7b178b1614b58b12d932bc2dbe8097b'
