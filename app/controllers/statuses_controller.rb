76ec6d3dfd597e592fff58773a67924ac6938e7c
Ken Mazaika <kenmazaika@gmail.com>
2012-10-06 18:03:39 -0400

    Make the creating API return a 201 Created, instead of 200 OK.  Add docs for the API's we built.

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -3,7 +3,7 @@ class StatusesController < ApplicationController
   def create
     status = Status.create(params[:status])
     if status.persisted?
-      render :json => status
+      render :json => status, :status => :created
     else
       render :json => {:errors => status.errors}, :status => :unprocessable_entity
     end
